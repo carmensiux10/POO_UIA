c347d8092937548d428a739eb9761bd998da974c
CARMEN <carmen_13666@hotmail.com>
2016-01-29 01:50:01 -0600

    Estructura del repositorio

---
+++ PRACTICAS/P01/PruebaApp.java
@@ -0,0 +1,23 @@
+public class PruebaApp {
+ 
+    public static void main(String[] args) {
+ 
+        int operador1 = 20;
+        int operador2 = 15;
+        int resultado=0;
+ 
+        resultado= operador1+operador2; //resultado = 35
+ 
+        resultado= operador1-operador2; //resultado = 5
+ 
+        resultado= operador2-operador1; //resultado = -5
+ 
+        resultado= operador1*operador2; //resultado = 300
+ 
+        resultado= operador1/operador2; //resultado = 1 (como es int no incluye decimales)
+ 
+        resultado= operador1%operador2; //resultado = 5 (el resto de la division)
+ 
+    }
+ 
+}
