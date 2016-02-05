8046961bb1df73798e8d0cadf314ed307b633ddb
CARMEN <carmen_13666@hotmail.com>
2016-02-05 00:09:45 -0600

    Estructura del repositorio

---
+++ PRACTICAS/P01/global_localc.java
@@ -0,0 +1,55 @@
+public class  global_localc{
+
+	byte pax=30;
+	short p_coffee=350;
+	int tips=1750;
+	long comidas2015=25480L;
+	char salon = 'C';
+	float estto=40.50F;
+	double total_evento= 10580;
+
+
+	public static void main (String [] args){
+	global_localc a = new global_localc();
+	byte precio_internet=30;
+	short pextra=350;
+	int roomnigth=1750;
+	long numpax2015=35000L;
+	char inicial = 'A';
+	float rate=1450.20F;
+	double room_revenue= 12545.5767;
+
+	System.out.println("TIPOS DE DATOS VARIABLE GLOBAL");
+	  System.out.println("TIPOS DE DATO BYTE VALE");
+      System.out.println( a.pax );
+      System.out.println("TIPOS DE DATO SHORT VALE");
+      System.out.println( a.p_coffee );
+      System.out.println("TIPOS DE DATO INT VALE");
+      System.out.println( a.tips);      
+      System.out.println("TIPOS DE DATO LONG VALE");
+      System.out.println( a.comidas2015);
+      System.out.println("TIPOS DE DATO CHAR VALE");
+      System.out.println( a.salon); 
+      System.out.println("TIPOS DE DATO FLOAT VALE");
+      System.out.println( a.estto);
+      System.out.println("TIPOS DE DATO DOUBLE VALE");
+      System.out.println( a.total_evento ); 
+
+      System.out.println("TIPOS DE DATOS VARIABLE LOCAL");
+       System.out.println("TIPOS DE DATO BYTE VALE");
+      System.out.println( precio_internet );
+      System.out.println("TIPOS DE DATO SHORT VALE");
+      System.out.println( pextra );
+      System.out.println("TIPOS DE DATO INT VALE");
+      System.out.println(  roomnigth );      
+      System.out.println("TIPOS DE DATO LONG VALE");
+      System.out.println( numpax2015);
+      System.out.println("TIPOS DE DATO CHAR VALE");
+      System.out.println( inicial );  
+      System.out.println("TIPOS DE DATO FLOAT VALE");
+      System.out.println( rate);
+      System.out.println("TIPOS DE DATO DOUBLE VALE");
+      System.out.println( room_revenue );  
+      }
+
+ }
---
+++ PRACTICAS/P01/tipo_datos.java
@@ -0,0 +1,33 @@
+public class  tipo_datos{
+
+	public static void main (String [] args){
+
+	byte precio_internet=30;
+	short pextra=350;
+	int roomnigth=1750;
+	long numpax2015=35000L;
+	char inicial = 'A';
+	float rate=1450.20F;
+	double room_revenue= 12545.5767;
+
+	System.out.println("TIPOS DE DATOS PRIMITIVOS ENTEROS");
+	  System.out.println("TIPOS DE DATO BYTE VALE");
+      System.out.println( precio_internet );
+      System.out.println("TIPOS DE DATO SHORT VALE");
+      System.out.println( pextra );
+      System.out.println("TIPOS DE DATO INT VALE");
+      System.out.println(  roomnigth );      
+      System.out.println("TIPOS DE DATO LONG VALE");
+      System.out.println( numpax2015);
+      System.out.println("TIPOS DE DATO CHAR VALE");
+      System.out.println( inicial );  
+    
+    System.out.println("TIPOS DE DATOS PRIMITIVOS REALES");
+
+     System.out.println("TIPOS DE DATO FLOAT VALE");
+     System.out.println( rate);
+     System.out.println("TIPOS DE DATO DOUBLE VALE");
+     System.out.println( room_revenue );  
+	}
+
+}
